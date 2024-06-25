ace7f3b124d60f0471b79c08b04fb9e0c349c15f
fahad <fah8ad@outlook.com>
2024-06-25 18:57:30 +0300

    updata some file

--- authUser/forms.py
+++ authUser/forms.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = User
-        fields =['username', 
-                 'first_name', 
-                 'last_name', 
-                 'email', 
-                 'password1', 
-                 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
